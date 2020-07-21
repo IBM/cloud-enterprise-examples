0edca0ecbfb231ed81443163048c7d83b5718b13
Tim Robinson <timroster@gmail.com>
2020-07-20 17:36:30 -0700

    switch to untyped lists

--- iac/20-ocp-vcp/variables.tf
+++ iac/20-ocp-vcp/variables.tf
@@ -8,15 +8,15 @@ variable "region" {
   default = "us-east"
 }
 variable "vpc_zone_names" {
-  type    = list(string)
+  type    = list
   default = ["us-east-1"]
 }
 variable "flavors" {
-  type    = list(string)
+  type    = list
   default = ["mx2.4x32"]
 }
 variable "workers_count" {
-  type    = list(number)
+  type    = list
   default = [2]
 }
 variable "k8s_version" {
