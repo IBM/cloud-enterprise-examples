59e2c5c10ef7844b416f0d7076292bf62eddbb38
Tim Robinson <timroster@gmail.com>
2020-08-11 20:56:28 -0700

    custom VPC address prefix

--- iac/20-ocp-vcp/network.tf
+++ iac/20-ocp-vcp/network.tf
@@ -1,5 +1,15 @@
 resource "ibm_is_vpc" "iac_iks_vpc" {
   name = "${var.project_name}-${var.environment}-vpc"
+  resource_group = data.ibm_resource_group.group.id
+  address_prefix_management = "manual"
+}
+
+resource "ibm_is_vpc_address_prefix" "vpc_address_prefix" {
+  count                     = local.max_size
+  name                      = "${var.project_name}-${var.environment}-range-${format("%02s", count.index)}"
+  zone                      = var.vpc_zone_names[count.index]
+  vpc                       = ibm_is_vpc.iac_iks_vpc.id
+  cidr                      = "172.26.${format("%01s", count.index)}.0/24"
 }
 
 resource "ibm_is_subnet" "iac_iks_subnet" {
@@ -7,9 +17,11 @@ resource "ibm_is_subnet" "iac_iks_subnet" {
   name                     = "${var.project_name}-${var.environment}-subnet-${format("%02s", count.index)}"
   zone                     = var.vpc_zone_names[count.index]
   vpc                      = ibm_is_vpc.iac_iks_vpc.id
-  public_gateway           = ibm_is_public_gateway.iac_iks_gateway[count.index].id
-  total_ipv4_address_count = 256
+  ipv4_cidr_block          = "172.26.${format("%01s", count.index)}.0/26"
+  # total_ipv4_address_count = 64
   resource_group           = data.ibm_resource_group.group.id
+  
+  depends_on  = [ibm_is_vpc_address_prefix.vpc_address_prefix]
 }
 
 resource "ibm_is_security_group_rule" "iac_iks_security_group_rule_tcp_k8s" {
