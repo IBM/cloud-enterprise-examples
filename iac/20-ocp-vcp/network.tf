773ad8ae6e61394e2688bb828bd2fc77008ae207
Tim Robinson <timroster@gmail.com>
2020-10-15 14:30:58 -0700

    add gateway to subnet

--- iac/20-ocp-vcp/network.tf
+++ iac/20-ocp-vcp/network.tf
@@ -20,6 +20,7 @@ resource "ibm_is_subnet" "iac_iks_subnet" {
   ipv4_cidr_block          = "172.26.${format("%01s", count.index)}.0/26"
   # total_ipv4_address_count = 64
   resource_group           = data.ibm_resource_group.group.id
+  public_gateway           = ibm_is_public_gateway.iac_iks_gateway[count.index]
   
   depends_on  = [ibm_is_vpc_address_prefix.vpc_address_prefix]
 }
