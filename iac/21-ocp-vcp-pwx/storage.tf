b5bfe1b2e0bad20c18a863081543e3cef160db83
Tim Robinson <timroster@gmail.com>
2020-12-15 16:40:46 -0800

    add cluster dependency

--- iac/21-ocp-vcp-pwx/storage.tf
+++ iac/21-ocp-vcp-pwx/storage.tf
@@ -1,4 +1,6 @@
 resource "ibm_is_volume" "iac_app_volume" {
+  depends_on = [ ibm_container_vpc_cluster.iac_iks_cluster ]
+
   count    = var.workers_count[0]
   resource_group = data.ibm_resource_group.group.id
   name     = "${var.project_name}-${var.environment}-volume-${format("%02s", count.index)}"
